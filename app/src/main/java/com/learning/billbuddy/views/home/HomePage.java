7006b5e0c77c725c20eda9de6d32e886c2f0d6a1
Aaron <aaron.dinh@shopback.com>
2025-01-06 21:11:13 +0700

    migrate create group to bottomsheet

--- app/src/main/java/com/learning/billbuddy/views/home/HomePage.java
+++ app/src/main/java/com/learning/billbuddy/views/home/HomePage.java
@@ -61,34 +61,38 @@ public class HomePage extends Fragment {
             @Override
             public void onClick(View v) {
                 FirebaseUser currentUser = mAuth.getCurrentUser();
-                if (currentUser != null) {
-                    String firebaseAuthID = currentUser.getUid();
-                    Log.d("HomePage", "Current Firebase Auth ID: " + firebaseAuthID);
-                    db.collection("users").whereEqualTo("userID", firebaseAuthID).get().addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            QuerySnapshot querySnapshot = task.getResult();
-                            if (querySnapshot != null && !querySnapshot.isEmpty()) {
-                                for (QueryDocumentSnapshot document : querySnapshot) {
-                                    User user = document.toObject(User.class);
-                                    if (user != null) {
-                                        Log.d("HomePage", "User found: " + user.getName());
-                                        Intent intent = new Intent(requireActivity(), AddGroupActivity.class);
-                                        intent.putExtra("OWNER_ID", user.getUserID());
-                                        intent.putExtra("OWNER_NAME", user.getName());
-                                        startActivity(intent);
-                                        Log.d("HomePage", "Starting AddGroupActivity");
-                                    }
-                                }
-                            } else {
-                                Log.d("HomePage", "No matching documents found");
-                            }
-                        } else {
-                            Log.d("HomePage", "get failed with ", task.getException());
-                        }
-                    });
-                } else {
+                if (currentUser == null) {
                     Log.d("HomePage", "No authenticated user found");
+                    return;
                 }
+                String firebaseAuthID = currentUser.getUid();
+                Log.d("HomePage", "Current Firebase Auth ID: " + firebaseAuthID);
+                db.collection("users").whereEqualTo("userID", firebaseAuthID).get().addOnCompleteListener(task -> {
+
+                    if (!task.isSuccessful()) {
+                        Log.d("HomePage", "get failed with ", task.getException());
+                        return;
+                    }
+
+                    QuerySnapshot querySnapshot = task.getResult();
+                    if (querySnapshot == null || querySnapshot.isEmpty()) {
+                        Log.d("HomePage", "No matching documents found");
+                        return;
+                    }
+
+                    for (QueryDocumentSnapshot document : querySnapshot) {
+                        User user = document.toObject(User.class);
+                        if (user != null) {
+                            Log.d("HomePage", "User found: " + user.getName());
+                            AddGroupBottomSheetDialog bottomSheet = new AddGroupBottomSheetDialog();
+                            Bundle args = new Bundle();
+                            args.putString("OWNER_ID", user.getUserID());
+                            args.putString("OWNER_NAME", user.getName());
+                            bottomSheet.setArguments(args);
+                            bottomSheet.show(requireActivity().getSupportFragmentManager(), "AddGroupBottomSheetDialog");
+                        }
+                    }
+                });
             }
         });
 
