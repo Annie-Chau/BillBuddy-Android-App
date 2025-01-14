36600169991edef249060b8539fd323c53bd3ce8
Aaron <aaron.dinh@shopback.com>
2025-01-14 20:01:03 +0700

    fix fragment

--- app/src/main/java/com/learning/billbuddy/views/expense/ViewGroupDetailActivity.java
+++ app/src/main/java/com/learning/billbuddy/views/expense/ViewGroupDetailActivity.java
@@ -17,6 +17,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -153,8 +155,13 @@ public class ViewGroupDetailActivity extends AppCompatActivity {
                 Bundle args = new Bundle();
                 args.putSerializable("group", currentGroup);
                 args.putDouble("amount", amount);
-                bottomSheet.setArguments(args);
-                bottomSheet.show(ViewGroupDetailActivity.this.getSupportFragmentManager(), "ViewReimbursementDetail");
+
+                FragmentManager fragmentManager = ViewGroupDetailActivity.this.getSupportFragmentManager();
+                Fragment oldFragment = fragmentManager.findFragmentByTag("ViewReimbursementDetail");
+                if (oldFragment == null || !oldFragment.isAdded()){
+                    bottomSheet.setArguments(args);
+                    bottomSheet.show(ViewGroupDetailActivity.this.getSupportFragmentManager(), "ViewReimbursementDetail");
+                }
             });
         });
 
